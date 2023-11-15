84e2ff7a7a4d048daaa3a20959faedf73347627f
soflil <134602526+soflil@users.noreply.github.com>
2023-11-15 10:20:54 +0100

    Added a footer, and created a search-form w/ the ability to target the input value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello world</header>
+      <div className="container">
+        <header className="App-header text-center">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Sophie Schou, open sourced on{" "}
+          <a
+            href="https://github.com/soflil/dictionary-app-react.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
